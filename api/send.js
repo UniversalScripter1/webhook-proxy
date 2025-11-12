430025bf5459f0dffda817c37282ac3681f6bb71
UniversalScripter1 <yeepeexd1@gmail.com>
2025-11-12 22:48:34 +0800

    Update Discord webhook URL to use environment variable

--- api/send.js
+++ api/send.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     // Vercel securely gives us the secret URL via environment variables
-    const DISCORD_WEBHOOK_URL = process.env.https://discord.com/api/webhooks/1393767292730609824/x0mWJyvh6vvyd-j2YIBhtRjOknEGydckSsCjFVFo7r5j_0SeVcu0mZW281dRmWuWKbXP;
+    const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK;
 
     if (!DISCORD_WEBHOOK_URL) {
         return res.status(500).json({ error: 'Server not configured.' });
